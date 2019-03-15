4b01f88d7b5c3b6bdff67d83c0e07e8d1557b9f8
Sellenite <714627528@qq.com>
2019-03-15 22:56:25 +0800

    update

---
+++ src/components/dynamic-button/dynamic-button.vue
@@ -0,0 +1,43 @@
+<template>
+<component :is="tagName" v-bind="tagProps" class="dynamic-button">
+    <slot></slot>
+</component>
+</template>
+
+<script>
+/* :is还可以绑定一个组件对象，也是常用的用法 */
+export default {
+    props: {
+        to: {
+            type: String,
+            default: ''
+        },
+        target: {
+            type: String,
+            default: '_self'
+        }
+    },
+    computed: {
+        tagName() {
+            return this.to === '' ? 'button' : 'a';
+        },
+        tagProps() {
+            let props = {};
+            if (this.to) {
+                props = {
+                    href: this.to,
+                    target: this.target
+                }
+            }
+
+            return props;
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.dynamic-button {
+    margin-right: 6px
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -2,15 +2,20 @@
     <div class="about">
         <h1>This is an about page</h1>
         <table-render :columns="columns" :data="data"></table-render>
+        <dynamic-button>按钮</dynamic-button>
+        <dynamic-button to="https://www.baidu.com">跳转链接</dynamic-button>
+        <dynamic-button to="https://www.baidu.com" target="_blank">跳转链接，新窗口</dynamic-button>
     </div>
 </template>
 
 <script>
     import tableRender from '../components/table/table-render.vue';
+    import dynamicButton from '../components/dynamic-button/dynamic-button.vue';
 
     export default {
         components: {
-            tableRender
+            tableRender,
+            dynamicButton
         },
         data() {
             return {
